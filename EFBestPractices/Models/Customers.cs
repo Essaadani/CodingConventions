4da0d808bdef00488bb50e0f829e1bbdbe83a1f8
Khalid ESSAADANI <khalid_essaadani@hotmail.fr>
2020-07-21 21:09:09 +0200

    EF Core Support

---
+++ EFBestPractices/Models/Customers.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFBestPractices.Models
+{
+    [NotMapped]
+    public partial class Customers
+    {
+
+
+
+        public Customers()
+        {
+            CustomerCustomerDemo = new HashSet<CustomerCustomerDemo>();
+            Orders = new HashSet<Orders>();
+        }
+
+        public string CustomerId { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+
+
+        [NotMapped]
+        public string PostalCode { get; set; }
+
+
+
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+
+        public virtual ICollection<CustomerCustomerDemo> CustomerCustomerDemo { get; set; }
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(string connectionString) 
+            : base(GetOptions(connectionString))
+        { }
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            return optionsBuilder.UseSqlServer(connectionString).Options;
+        }
+    }
+}
---
+++ EFBestPractices/Program.cs
@@ -0,0 +1,151 @@
+﻿using EFBestPractices.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace EFBestPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region FilterOnServer
+            // Before
+
+            //using (NorthwindContext context = new NorthwindContext())
+            //{
+            //    var customers = from customer in context.Customers
+            //                    select customer;
+
+            //    int count = 0;
+            //    foreach (var customer in customers)
+            //    {
+            //        if (customer.Country == "Germany")
+            //        {
+            //            Console.WriteLine(customer.ContactName);
+            //            count++;
+            //        }
+            //    }
+
+            //    Console.WriteLine($"Records count: {count}");
+            //    Console.ReadKey();
+            //}
+
+
+            // After (Filter on Server)
+            //using (NorthwindContext context = new NorthwindContext())
+            //{
+            //    var customers = from customer in context.Customers
+            //                    where customer.Country.Equals("Germany")
+            //                    select customer;
+
+            //    foreach (var customer in customers)
+            //    {
+            //        Console.WriteLine(customer.ContactName);
+            //    }
+
+            //    Console.ReadKey();
+            //}
+            #endregion
+
+            #region UseProjection
+            //using (NorthwindContext context = new NorthwindContext())
+            //{
+            //    // Without Projection
+            //    Stopwatch stopwatch = new Stopwatch();
+            //    stopwatch.Start();
+            //    var customers = from customer in context.Customers
+            //                    select customer;
+
+            //    foreach (var customer in customers)
+            //    {
+            //        Console.WriteLine(customer.ContactName);
+            //    }
+
+            //    stopwatch.Stop();
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.WriteLine("Before projection: " + stopwatch.ElapsedMilliseconds);
+            //    Console.ForegroundColor = ConsoleColor.White;
+
+            //    // Using Projection
+            //    stopwatch.Restart();
+            //    var customerNames = from customer in context.Customers
+            //                    select new { customer.ContactName};
+
+            //    foreach (var customer in customerNames)
+            //    {
+            //        Console.WriteLine(customer.ContactName);
+            //    }
+
+            //    stopwatch.Stop();
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //    Console.WriteLine("After projection: " + stopwatch.ElapsedMilliseconds);
+
+            //    Console.ReadKey();
+            //}
+
+            #endregion
+
+            #region UseEagerLoading
+
+            //using (NorthwindContext context = new NorthwindContext())
+            //{
+            //    var customers =
+            //        context.Customers
+            //        .Include(x => x.Orders)
+            //        .ThenInclude(y => y.OrderDetails);
+
+            //    foreach (var customer in customers)
+            //    {
+            //        Console.WriteLine($"Customer Name: {customer.ContactName}");
+            //        Console.WriteLine("-----------------------------");
+            //        foreach (var order in customer.Orders)
+            //        {
+            //            Console.WriteLine($"\tOrder Date: {order.OrderDate}");
+            //            Console.WriteLine("\t-----------------------------");
+            //            foreach (var detail in order.OrderDetails)
+            //            {
+            //                Console.WriteLine($"\t\tProduct Id: {detail.ProductId}");
+            //            }
+            //        }
+            //    }
+            //    Console.ReadKey();
+            //}
+
+
+            #endregion
+
+            #region TrackEntities
+            //using (NorthwindContext context = new NorthwindContext())
+            //{
+
+            //    var customers =
+            //        context.Customers
+            //        .Where(x => x.ContactName.StartsWith("A"));
+
+            //    Console.WriteLine($"Customers count: {customers.Count()}");
+
+            //    foreach (var customer in customers)
+            //    {
+            //        customer.Country = "Morocco";
+            //    }
+
+            //    var modifiedCustomers =
+            //        context.ChangeTracker
+            //        .Entries<Customers>()
+            //        .Where(x => x.State == EntityState.Modified)
+            //        .Select(x => x.Entity);
+
+            //    Console.WriteLine($"Modified customers count: {modifiedCustomers.Count()}");
+
+            //    Console.ReadKey();
+            //}
+            #endregion
+
+
+        }
+
+
+    }
+}
